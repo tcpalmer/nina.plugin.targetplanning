648ea26f1588c9a3fbd39d2c83185366a6234754
Tom Palmer <palmer.tom@gmail.com>
2022-11-12 05:14:49 -0500

    rough 3

--- NINA.Plugin.TargetPlanning/NINA.Plugin.TargetPlanning/AnnualChart/AnnualPlanningChartModel.cs
+++ NINA.Plugin.TargetPlanning/NINA.Plugin.TargetPlanning/AnnualChart/AnnualPlanningChartModel.cs
@@ -1,23 +1,23 @@
 ﻿
 using OxyPlot;
-using OxyPlot.Series;
-using System;
+using System.Collections.Generic;
 
 namespace TargetPlanning.NINAPlugin.AnnualChart {
 
     public class AnnualPlanningChartModel {
 
-        private PlotModel _chart;
-        public PlotModel Chart {
-            get => _chart;
-            set {
-                _chart = value;
-            }
-        }
+        public IList<DataPoint> Points { get; private set; }
 
         public AnnualPlanningChartModel() {
-            Chart = new PlotModel { Title = "Example 1" };
-            Chart.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+
+            Points = new List<DataPoint> {
+                new DataPoint(0, 4),
+                new DataPoint(10, 13),
+                new DataPoint(20, 15),
+                new DataPoint(30, 16),
+                new DataPoint(40, 12),
+                new DataPoint(50, 12)
+            };
         }
 
     }
