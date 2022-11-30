b0d6d41b6fd458e43518f873f08249735a07b830
Tom Palmer <palmer.tom@gmail.com>
2022-11-30 14:25:19 -0500

    wip

--- NINA.Plugin.TargetPlanning/NINA.Plugin.TargetPlanning/ImagingSeason/ImagingSeasonChartModel.cs
+++ NINA.Plugin.TargetPlanning/NINA.Plugin.TargetPlanning/ImagingSeason/ImagingSeasonChartModel.cs
@@ -3,6 +3,8 @@ using OxyPlot;
 using OxyPlot.Axes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using TargetPlanning.NINAPlugin.Astrometry;
 
 namespace TargetPlanning.NINAPlugin.ImagingSeason {
@@ -12,6 +14,8 @@ namespace TargetPlanning.NINAPlugin.ImagingSeason {
         public string TargetName { get; private set; }
         public IList<DataPoint> ImagingMinutes { get; private set; }
         public IList<bool> Accepted { get; private set; }
+        public string AnnoText { get; private set; }
+        public DataPoint AnnoPoint { get; private set; }
 
         private long _maxImagingMinutes;
         public long MaxImagingMinutes {
@@ -27,13 +31,24 @@ namespace TargetPlanning.NINAPlugin.ImagingSeason {
             foreach (ImagingDayPlan plan in results) {
                 bool accepted = !plan.StartLimitingFactor.Session;
                 long imagingMinutes = accepted ? plan.GetImagingMinutes() : 0;
-                DateTime displayImagingMinutes = new DateTime(plan.StartDay.Year, plan.StartDay.Month, plan.StartDay.Day).AddMinutes(imagingMinutes);
-                ImagingMinutes.Add(new DataPoint(DateTimeAxis.ToDouble(plan.StartDay), imagingMinutes));
+                ImagingMinutes.Add(new DataPoint(DateTimeAxis.ToDouble(plan.StartDay), TimeSpanAxis.ToDouble(TimeSpan.FromMinutes(imagingMinutes))));
                 Accepted.Add(accepted);
                 if (imagingMinutes > _maxImagingMinutes) {
                     _maxImagingMinutes = imagingMinutes;
                 }
             }
+
+            int startYear = results.First().StartDay.Year;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(startYear.ToString());
+            int endYear = results.Last().StartDay.Year;
+            if (startYear != endYear) {
+                sb.Append("-").Append(endYear.ToString());
+            }
+
+            AnnoText = sb.ToString();
+            DateTime dt = results.First().StartDay.AddDays(15);
+            AnnoPoint = new DataPoint(DateTimeAxis.ToDouble(dt), TimeSpanAxis.ToDouble(TimeSpan.FromMinutes(MaxImagingMinutes * .87)));
         }
     }
 
---
+++ NINA.Plugin.TargetPlanning/NINA.Plugin.TargetPlanning/ImagingSeason/PlotModelTest.cs
@@ -0,0 +1,32 @@
+﻿using NINA.Core.Utility;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace TargetPlanning.NINAPlugin.ImagingSeason {
+
+    public class PlotModelTest : PlotModel {
+
+        public PlotModelTest() : base() {
+            Title = "PlotModel Test";
+            PlotAreaBorderColor = OxyColors.Transparent;
+            Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
+            Axes.Add(new LinearAxis { Position = AxisPosition.Left });
+
+            LineSeries ls = new LineSeries();
+            ls.Title = "My Line Series";
+            ls.MarkerType = MarkerType.Circle;
+            ls.Points.Add(new DataPoint(0, 0));
+            ls.Points.Add(new DataPoint(10, 18));
+            ls.Points.Add(new DataPoint(20, 12));
+            ls.Points.Add(new DataPoint(30, 8));
+            ls.Points.Add(new DataPoint(40, 15));
+
+            Series.Add(ls);
+
+            // this.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+            Logger.Debug("NEW PlotModelTest");
+        }
+    }
+
+}
